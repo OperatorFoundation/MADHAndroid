1e21e3666b399f15c76a2fe963ce71bd581d1076
consuelita <adelita@operatorfoundation.org>
2025-10-22 20:08:47 -0500

    Added Remaining Types

--- MADH/src/main/java/org/operatorfoundation/madh/types/Confirmation.kt
+++ MADH/src/main/java/org/operatorfoundation/madh/types/Confirmation.kt
@@ -1,19 +1,19 @@
 package org.operatorfoundation.madh.types
 
 /**
- * Represents a unique session identifier used in MA-DH key confirmation.
+ * Represents a confirmation value used in MA-DH key confirmation.
  */
-data class SessionIdentifier(val bytes: ByteArray)
+data class Confirmation(val bytes: ByteArray)
 {
     override fun equals(other: Any?): Boolean {
         if (this == other) return true
-        if (other !is SessionIdentifier) return false
+        if (other !is Confirmation) return false
 
         return bytes.contentEquals(other.bytes)
     }
 
     override fun hashCode(): Int
     {
-        return bytes.hashCode()
+        return bytes.contentHashCode()
     }
 }
